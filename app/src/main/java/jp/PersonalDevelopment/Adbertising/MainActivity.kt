44b791539dcd3008ffc07fa36efc88f7e71f335f
KoukiKimura <pengin24.purupuru@gmail.com>
2020-11-12 15:44:40 +0900

    second

--- app/src/main/java/jp/PersonalDevelopment/Adbertising/MainActivity.kt
+++ app/src/main/java/jp/PersonalDevelopment/Adbertising/MainActivity.kt
@@ -3,6 +3,7 @@ package jp.PersonalDevelopment.Adbertising
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener =
@@ -28,7 +29,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        navigation.setOnNavigationItemSelectedListener(mOnNvigationItemSelectedListener)
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
 
