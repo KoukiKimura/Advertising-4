c49bd44735bd0f3359f788ab41b941faf833db65
KoukiKimura <pengin24.purupuru@gmail.com>
2020-11-18 15:29:31 +0900

    thread commit

--- app/src/main/java/jp/PersonalDevelopment/Adbertising/MainActivity.kt
+++ app/src/main/java/jp/PersonalDevelopment/Adbertising/MainActivity.kt
@@ -1,8 +1,10 @@
 package jp.PersonalDevelopment.Adbertising
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import jp.PersonalDevelopment.Adbertising.ui.login.LoginActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +12,8 @@ class MainActivity : AppCompatActivity() {
         BottomNavigationView.OnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.navigation_home -> {
-                    message.setText(R.string.title_home)
-                    return@OnNavigationItemSelectedListener true
+                    val intent = Intent(this, LoginActivity::class.java )
+                    startActivity(intent)
                 }
                 R.id.navigation_dashboard -> {
                     message.setText(R.string.title_dashboard)
@@ -29,9 +31,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragment_container, firstFragment)
+        fragmentTransaction.commit()
+
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
-
-
-
 }
