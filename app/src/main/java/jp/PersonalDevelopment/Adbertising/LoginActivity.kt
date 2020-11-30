560c326a8c988de314f94ca748036d989b422d88
KoukiKimura <pengin24.purupuru@gmail.com>
2020-11-30 16:44:11 +0900

    firebase : loginform 2 2020/11/30

--- app/src/main/java/jp/PersonalDevelopment/Adbertising/LoginActivity.kt
+++ app/src/main/java/jp/PersonalDevelopment/Adbertising/LoginActivity.kt
@@ -2,19 +2,25 @@ package jp.PersonalDevelopment.Adbertising
 
 import android.content.Context
 import android.os.Bundle
-import android.preference.PreferenceManager
+import android.preference.*
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.*
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import jp.PersonalDevelopment.Adbertising.Adbertising.NameKEY
 import jp.PersonalDevelopment.Adbertising.Adbertising.UsersPATH
 import kotlinx.android.synthetic.main.activity_login.*
 
+import java.util.HashMap
 
 class LoginActivity : AppCompatActivity() {
 
--- app/src/main/java/jp/PersonalDevelopment/Adbertising/MainActivity.kt
+++ app/src/main/java/jp/PersonalDevelopment/Adbertising/MainActivity.kt
@@ -1,44 +1,49 @@
 package jp.PersonalDevelopment.Adbertising
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
+import android.util.TypedValue
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_home.*
 
+
 class MainActivity : AppCompatActivity(){
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-            Log.d("MainActivity","NavigationView")
+            Log.d("MainActivity", "NavigationView")
 
             when (item.itemId) {
                 R.id.navigation_Home -> {
                     Log.d("MainActivity", "Home")
                     supportFragmentManager.beginTransaction()
-                            .replace(R.id.fragment_container, HomeFragment())
-                            .commit()
+                        .replace(R.id.fragment_container, HomeFragment())
+                        .commit()
                     return@OnNavigationItemSelectedListener true
                 }
                 R.id.navigation_Search -> {
                     Log.d("MainActivity", "Search")
                     supportFragmentManager.beginTransaction()
-                            .replace(R.id.fragment_container, SearchFragment())
-                            .commit()
+                        .replace(R.id.fragment_container, SearchFragment())
+                        .commit()
                     return@OnNavigationItemSelectedListener true
                 }
                 R.id.navigation_Coupon -> {
                     Log.d("MainActivity", "Coupon")
                     supportFragmentManager.beginTransaction()
-                            .replace(R.id.fragment_container, CouponFragment())
-                            .commit()
+                        .replace(R.id.fragment_container, CouponFragment())
+                        .commit()
                     return@OnNavigationItemSelectedListener true
                 }
                 R.id.navigation_Subscribe -> {
                     Log.d("MainActivity", "Subscibe")
                     supportFragmentManager.beginTransaction()
-                            .replace(R.id.fragment_container, SubscribeFragment())
-                            .commit()
+                        .replace(R.id.fragment_container, SubscribeFragment())
+                        .commit()
                     return@OnNavigationItemSelectedListener true
                 }
             }
@@ -48,6 +53,8 @@ class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigation.setEnabled(true);
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         supportFragmentManager.beginTransaction()
